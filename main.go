47c1a99780b24f4ce647d845abab51078510b343
santhosh <ssanthoshss98@gmail.com>
2022-12-14 00:12:15 +0530

    Added Encrypted text to file function

--- main.go
+++ main.go
@@ -1,24 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/santo-98/godb/utils/fileoperations"
 )
 
 func main() {
-	f, err := os.Create("test.godb")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Created test file")
-
-	_, writeErr := f.WriteString("testing")
-	if writeErr != nil {
-		fmt.Println(writeErr)
-	}
-
-	fmt.Println(fileoperations.ReadEncryptedFile)
-	defer f.Close()
+	fileoperations.WriteEncryptedFile()
 }
--- utils/fileoperations/fileoperations.go
+++ utils/fileoperations/fileoperations.go
@@ -1,7 +1,50 @@
 package fileoperations
 
-import "fmt"
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+	"os"
+)
+
+func getKeys() []byte {
+	key, err := os.ReadFile("config/keys")
+	if err != nil {
+		panic(err)
+	}
+
+	return key
+}
 
 func ReadEncryptedFile() {
-	fmt.Println("here")
+	key := getKeys()
+	fmt.Println(string(key))
+}
+
+func WriteEncryptedFile() {
+	key := getKeys()
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err)
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, []byte("plaintext"), nil)
+
+	err = os.WriteFile("test.godb", ciphertext, 0777)
+	if err != nil {
+		panic(err)
+	}
 }
