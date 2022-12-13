dbceb01aa57324fb77c107bf97adf08da67208f7
santhosh <ssanthoshss98@gmail.com>
2022-12-13 19:04:40 +0530

    created new file

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Here")
+	f, err := os.Create("test.godb")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Created test file")
+	defer f.Close()
+
 }
